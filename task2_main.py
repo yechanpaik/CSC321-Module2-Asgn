195cb2068490d446e7942cca4dbbb7d47e17f2c1
KrakenM (T) <moemyatag05@gmail.com>
2025-09-28 01:27:07 -0700

    submit() function complete
    Co-authored-by: Sean Paik <yechanpaik@users.noreply.github.com>
    Co-authored-by: briggsmurphy <briggsmurphy@users.noreply.github.com>

--- task2_main.py
+++ task2_main.py
@@ -1,19 +1,14 @@
-# --- Task 2
+def submit(input_string: str) -> bytes:
+    prepended_string = "userid=456;userdata=" + input_string + ";session-id=31337"
+    # url_encoded_string = prepended_string.replace("=", "%3D").replace(";", "%3B")
+    url_encoded_string = quote(prepended_string, safe='') #replaces special characters with url encodings dynamically
+    
+    utf_encoded_string = url_encoded_string.encode('utf-8')
+    
+    print(f"utf: {utf_encoded_string}")
+    padded_string_bytes: bytes = pad(utf_encoded_string, 16)
 
-# region BEGIN SUBMIT()
-original_string = input("Enter a string: ")
+    plaintext_blocks = [padded_string_bytes[i:i+16] for i in range(0, len(padded_string_bytes), 16)]
 
-prepended_string = "userid=456; userdata=" + original_string
-
-appended_string = prepended_string + ";session-id=31337"
-
-url_encoded_string_first_half = appended_string.replace("=", "%3D")
-url_encoded_string = url_encoded_string_first_half.replace(";", "%3B")
-
-padded_string = pad(url_encoded_string.encode('utf-8'), 16)
-
-# endregion SUBMIT()
-
-#region BEGIN VERIFY()
-
-#endregion VERIFY()
+    ciphertext = encrypt_with_cbc(key, iv, plaintext_blocks)
+    return ciphertext
