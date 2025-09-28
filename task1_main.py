1a9d597d7215379a049446a24ef3dc71baf7f998
KrakenM (T) <moemyatag05@gmail.com>
2025-09-27 23:23:53 -0700

    Refactor

--- task1_main.py
+++ task1_main.py
@@ -4,73 +4,88 @@ from Crypto.Util.Padding import pad
 from Crypto.Util.strxor import strxor
 
 KEY_LEN_PLACEHOLDER = int(128/8)
-#Task 1
-#part 1: program takes in a plaintext file (.txt)
-plaintext_blocks = []
-filename = input("Enter the bmp name: ")
-header: bytearray = None
-with open(filename, 'rb') as file:
-    header = file.read(54)
-    # header
-    contents = file.read(16)
-    while(contents):
-        if(len(contents) < 16): 
-            padding = bytes([16-len(contents)]) * (16-len(contents)) #pads with the value: number of padding required
-            padded_plaintext = contents + padding
-            plaintext_blocks.append(padded_plaintext)
+
+def encrypt_with_ecb(key: bytes, plaintext_blocks: list[bytes]):
+    #key: a 128bit random byte that acts as the key
+    #plaintext_blocks: an array of plaintext 16 bytes ea
+    #RETURNS complete ciphertext
+    
+    #Cipher: a sequence of operations that will convert plaintext into ciphertext, 
+    #   essentially handling the 'encryption' of the plaintext
+    cipher_ecb = AES.new(key, AES.MODE_ECB) # AES.new() generates a cipher
+
+    ciphertext_blocks_ecb = []
+    for blocks in plaintext_blocks:
+        encrypted_block_ecb = cipher_ecb.encrypt(blocks)
+        ciphertext_blocks_ecb.append(encrypted_block_ecb)
+        
+    ciphertext_ecb = b''.join(ciphertext_blocks_ecb) #use the array to build a ciphertext string
+
+    print(f"encrypted ciphertext (ECB): \n{ciphertext_ecb}\n")
+    return ciphertext_ecb
+    
+def encrypt_with_cbc(iv: bytes, plaintext_blocks: list[bytes]): 
+    #key: a 128bit random byte that acts as the key
+    #plaintext_blocks: an array of plaintext 16 bytes ea
+    #RETURNS complete ciphertext
+    
+    cipher_cbc = AES.new(iv, AES.MODE_CBC)
+
+    ciphertext_blocks_cbc = []
+        
+    i = 0
+    for blocks in plaintext_blocks:
+        if (i==0):
+            one_before = blocks
+            encrypted_block_cbc = cipher_cbc.encrypt(strxor(blocks, iv))
+            i = 1
         else:
-            plaintext_blocks.append(contents)
+            encrypted_block_cbc  = cipher_cbc.encrypt(strxor(blocks, one_before))
+            one_before = blocks
+        ciphertext_blocks_cbc.append(encrypted_block_cbc)
+
+    ciphertext_cbc = b''.join(ciphertext_blocks_cbc)
+
+    print(f"encrypted ciphertext (CBC): \n{ciphertext_cbc}\n")
+    return ciphertext_cbc
+
+def main():
+    #Task 1
+    #part 1: program takes in a plaintext file (.txt)
+    plaintext_blocks = []
+    filename = input("Enter the bmp name: ")
+    header: bytearray = None
+
+    #break plaintext down into plaintext[]: an array of strings 128bits each
+    #with padding as neccessary or at the end
+
+    with open(filename, 'rb') as file:
+        header = file.read(54)
+        # header
         contents = file.read(16)
+        while(contents):
+            if(len(contents) < 16): 
+                padding = bytes([16-len(contents)]) * (16-len(contents)) #pads with the value: number of padding required
+                padded_plaintext = contents + padding
+                plaintext_blocks.append(padded_plaintext)
+            else:
+                plaintext_blocks.append(contents)
+            contents = file.read(16)
 
-#break file down into plaintext[]: an array of strings 128bits each || Blocks
-#   with padding as neccessary or at the end
-   
-print(f"parsed array of plaintext_blocks: \n{plaintext_blocks}\n")
+    print(f"parsed array of plaintext_blocks: \n{plaintext_blocks}\n")
 
-# region BEGIN ECB
-key = get_random_bytes(KEY_LEN_PLACEHOLDER)
-
-#a Cipher is a sequence of operations that will convert plaintext into ciphertext, 
-#   essentially handling the 'encryption' of the plaintext
-#AES.new() generates a cipher
-cipher_ecb = AES.new(key, AES.MODE_ECB)
-
-ciphertext_blocks_ecb = []
-for blocks in plaintext_blocks:
-    encrypted_block_ecb = cipher_ecb.encrypt(blocks)
-    ciphertext_blocks_ecb.append(encrypted_block_ecb)
-    
-ciphertext_ecb = b''.join(ciphertext_blocks_ecb) #use the array to build a ciphertext string
-
-print(f"final ciphertext_ecb: \n{ciphertext_ecb}\n")
-with open("encrypted_ecb_" + filename, 'wb') as file:
-    file.write(header)
-    file.write(ciphertext_ecb)
-    
-# endregion ECB
-
-# region BEGIN CBC 
-cipher_cbc = AES.new(key, AES.MODE_CBC)
-
-ciphertext_blocks_cbc = []
-    
-cbc_iv = get_random_bytes(KEY_LEN_PLACEHOLDER)
-i = 0
-for blocks in plaintext_blocks:
-  if(i==0):
-      one_before = blocks
-      encrypted_block_cbc = cipher_cbc.encrypt(strxor(blocks, cbc_iv))
-      i = 1
-  else:
-      encrypted_block_cbc  = cipher_cbc.encrypt(strxor(blocks, one_before))
-      one_before = blocks
-  ciphertext_blocks_cbc.append(encrypted_block_cbc)
-
-ciphertext_cbc = b''.join(ciphertext_blocks_cbc)
-
-print(f"final ciphertext_cbc: \n{ciphertext_cbc}\n")
-with open("encrypted_cbc_" + filename, 'wb') as file:
-    file.write(header)
-    file.write(ciphertext_cbc)
-    
-# endregion END CBC
+    key = get_random_bytes(KEY_LEN_PLACEHOLDER)
+    ciphertext_ecb = encrypt_with_ecb(key, plaintext_blocks)
+    with open("encrypted_ecb_" + filename, 'wb') as file:
+        file.write(header)
+        file.write(ciphertext_ecb)
+        
+    cbc_iv = get_random_bytes(KEY_LEN_PLACEHOLDER)
+    ciphertext_cbc = encrypt_with_cbc(cbc_iv, plaintext_blocks)
+    with open("encrypted_cbc_" + filename, 'wb') as file:
+        file.write(header)
+        file.write(ciphertext_cbc)
+        
+if __name__ == "__main__":
+    main()
+        
